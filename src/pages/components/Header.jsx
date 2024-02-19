05d6d807dbc5d0927dc1c04117c24c342f1bb1d1
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-19 11:20:59 +0200

    update
    update

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
             <div className="tweet-editor-form">
                <input 
                type="text" 
-               placeholder="What's happening" 
+               placeholder="What's happening ?" 
                className="tweet-editor-input" />
                <div className="tweet-editor-buttons">
                   <div className="tweet-editor-actions">
