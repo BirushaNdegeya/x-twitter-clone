578157c29441a934649c2ce85a90588d4db2a3b2
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-19 10:19:34 +0200

    update
    update

--- src/data/ImagesIcons.jsx
+++ src/data/ImagesIcons.jsx
@@ -1,7 +1,15 @@
 import birusha from '../assets/birusha.jpg';
 import { FaWandMagicSparkles } from "react-icons/fa6";
+import { IoImageOutline } from "react-icons/io5";
+import { MdOutlineGifBox } from "react-icons/md";
+import { BsEmojiSmile } from "react-icons/bs";
+import { LuCalendarClock } from "react-icons/lu";
 
 export {
    birusha,
    FaWandMagicSparkles,
+   IoImageOutline,
+   MdOutlineGifBox,
+   BsEmojiSmile,
+   LuCalendarClock,
 }
