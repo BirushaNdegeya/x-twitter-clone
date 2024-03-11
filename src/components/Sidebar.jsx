a31d909b565d07000fbd1b15111bd71185eb2a63
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-03-11 09:27:35 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "./buttons/Button";
 
 const Sidebar = () => {
    return (
       <div className="sidebar">
          <h1>Side bar</h1>
+         <Button titre='Tweet' />
       </div>
    );
 };
