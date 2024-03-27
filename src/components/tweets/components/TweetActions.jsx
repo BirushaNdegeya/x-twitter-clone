56608bc18755f317e01fe6473506f161ba88bf36
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-03-27 09:34:19 +0300

    Added functionality for comment and post buttons interaction
    Implemented the necessary code to enable interaction between the comment and post buttons. Users can now seamlessly post comments on threads and engage with discussions, enhancing the platform's usability and fostering community interaction.

--- src/components/tweets/components/TweetActions.jsx
+++ src/components/tweets/components/TweetActions.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-import { 
-   share,
-   love,
-   retweet,
-   reply
-} from '../../../data/ImagesIcons';
+import React, { useState } from "react";
+import Reply from "../../../assets/customs/Reply";
+import ReactIcon from "../../../assets/customs/ReactIcon";
+import Retweet from "../../../assets/customs/Retweet";
+import Share from "../../../assets/customs/Share";
 import TweetAction from "./TweetAction";
 
 const TweetActions = ({ replyTxt, retweetTxt, loveTxt}) => {
-   const handleLove = () => {
-
-   }
    return (
       <div className="tweet-actions">
-         <TweetAction icon={reply} text={replyTxt} />
-         <TweetAction icon={retweet} text={retweetTxt} />
-         <TweetAction 
-            icon={love} 
-            text={loveTxt}
-            handleClick={handleLove} 
-         />
-         <TweetAction icon={share} />
+         
+         <abbr title="Like" className="none">
+            <TweetAction 
+               svgBox={
+                  <Reply />
+               }
+               text={replyTxt}   
+            />
+         </abbr>
+         
+         <abbr title="Retweet" className="none">
+            <TweetAction 
+               svgBox={
+                  <Retweet />
+               } 
+               text={retweetTxt} 
+            />
+         </abbr>
+
+         <abbr title="React" className="none">
+            <TweetAction 
+               svgBox={<ReactIcon />} 
+               text={loveTxt}
+            />
+         </abbr>
+         
+         <abbr title="Share" className="none">
+            <TweetAction 
+               svgBox={
+                  <Share />
+               } 
+            /> 
+         </abbr>
+
       </div>
    );
 };
